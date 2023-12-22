d42eabbdca2ea94f09858a66b4100c8b71d58a55
omarstar <obarakat12@gmail.com>
2023-12-22 13:11:46 +0400

    cart view updates

--- src/components/cart/CartView.js
+++ src/components/cart/CartView.js
@@ -3,17 +3,17 @@ import "./cartview.css"
 import { useEffect } from "react";
 import { cartActions } from "../../store/cart/cart-slice";
 import { getFormatAmount } from "../../utils/helpers";
+import { Link } from "react-router-dom";
 
 const CartView = () => {
 
     // const isCartOpen =  useSelector(state => state.cart.showCartView);
-    const isCartOpen = true;
-    const subtotalAmount = 3964;
 
     const cart = useSelector((state) => state.cart);
     const dispatch = useDispatch();
+    const {cartTotalQuantity, cartTotalAmount, showCartView} = cart;
 
-    const {getTotals, addToCart, decreaseCart, removeFromCart} = cartActions;
+    const {getTotals, addToCart, decreaseCart, removeFromCart, setHideCartView} = cartActions;
 
     useEffect(() => {
         dispatch(getTotals());
@@ -29,48 +29,63 @@ const CartView = () => {
         dispatch(removeFromCart(product));
     };
 
+    
+
     return ( 
-        <div className={`cart_drawer ${isCartOpen ? 'is-open' : ''}`}>
+        <div className={`cart_drawer ${showCartView ? 'is-open' : ''}`}>
             <div class="cart-drawer_inner">
                 <div className="cart-drawer_header">
                     <div className="cart-drawer__headerInner">
-                        <div class="cart-drawer__icon"><span class="icon-cart"><a href="/cart"><i class="fas fa-shopping-cart" aria-hidden="true"></i></a></span></div>
+                        <div class="cart-drawer__icon"><span class="icon-cart">
+                            <Link to="/balloons/cart"><i class="fas fa-shopping-cart" aria-hidden="true"></i></Link></span>
+                        </div>
                         <div className="cart-drawer__title"> Cart </div>
-                        <div class="cart-drawer__close js-cart-drawer-close"><i class="fas fa-times" aria-hidden="true"></i></div>
+                        <div onClick={()=>dispatch(setHideCartView())} class="cart-drawer__close js-cart-drawer-close"><i class="fas fa-times" aria-hidden="true"></i></div>
                     </div>
                 </div>
                 <div className="cart-drawer_content">
                     {/* list of cart */}
                     {
-                        cart.cartItems &&
+                        cart.cartItems && cartTotalQuantity !== 0 ?
                         cart.cartItems.map((cartItem, cid) => (
                             <div className="cart-drawer-item__single" data-line={cid} key={cid}>
+                                {/* link to product detail */}
                                 <div className="cart-item__image">
                                     <a href="/products/giant-gold-latex-color-latex-balloon-helium-inflated-1?variant=43503323349214">
                                         <img class="cart-item__image" src="https://cdn.shopify.com/s/files/1/0508/6576/6572/products/GLH002.jpg?v=1665303387" alt={cartItem.name} />
                                     </a>
                                 </div>
                                 <div class="cart-item__content">
+                                {/* link to product detail */}
                                     <div class="cart-item__title">
                                         <a href="/products/giant-gold-latex-color-latex-balloon-helium-inflated-1?variant=43503323349214">{cartItem.name} </a>
                                     </div>
                                     <div class="innerwrap">
                                         <div class="cart-item_priceRemove">
-                                            <div class="cart-item__price">{getFormatAmount(cartItem.price)}</div>
-                                            <div class="cart-item__remove" data-line="1">
+                                            <div class="cart-item__price">{getFormatAmount(cartItem.price * cartItem.cartQuantity)}</div>
+                                            <div onClick={()=>handleRemoveFromCart(cartItem)} class="cart-item__remove" data-line="1">
                                                 <span class="remove-text js-remove-from-cart">Remove</span>
-                                                <span class="remove-icon"></span>
+                                                {/* <span class="remove-icon"></span> */}
                                             </div>
                                         </div>
                                         <div class="cart-item__quantity" data-line="43503323349214">
-                                            <span class="cart-item__minus-quantity" data-field="quantity[]">-</span>
-                                            <input type="text" class="cart-item__input-quantity" value="1" min="1" pattern="[0-9]*" name="qauntity[]"/>
-                                                <span class="cart-item__add-quantity" data-field="quantity[]">+</span>
+                                            <span onClick={()=>handleDecreaseCart(cartItem)} class="cart-item__minus-quantity" data-field="quantity[]">-</span>
+                                            <input type="text" class="cart-item__input-quantity" value={cartItem.cartQuantity} min="1" pattern="[0-9]*" name="qauntity[]"/>
+                                                <span onClick={()=>handleAddToCart(cartItem)} class="cart-item__add-quantity" data-field="quantity[]">+</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        ))
+                        )) 
+                        : (
+                            <div className="cart-drawer-item__single">
+                                {/* <p>Your cart is currently empty</p> */}
+                                <div className="start-shopping wide text-center">
+                                    
+                                    <span> Your cart is currently empty.</span>
+                                </div>
+                            </div>
+                        )
                     }
                     
                     {/* <div className="cart-drawer-item__single" data-line="1">
@@ -101,18 +116,31 @@ const CartView = () => {
                     </div> */}
                 </div>
                 <div className="cart-drawer_footer">
-                    list of buttons and actions
                     <div className="cart-drawer__subtotal">
                         <label>Sub total</label>
-                        <span>AED {subtotalAmount}</span>
+                        <span>AED {cartTotalAmount} | {cartTotalQuantity} Qty</span>
                     </div>
                     <div className="cart-drawer__buttons">
-                        <a href="/cart" class="button js-button js-checkout-button continueshopping">
+                        <a href="/balloons/cart" class="button js-button js-checkout-button continueshopping">
                             <span>Checkout Now</span>
                         </a>
-                        <a href="/collections/all" class="button js-button js-checkout-button cart-drawer__close js-cart-drawer-close">
-                            <span>Continue Shopping</span>
-                        </a>
+                        <button onClick={()=>dispatch(setHideCartView())} class="button js-button js-checkout-button cart-drawer__close js-cart-drawer-close">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                className="bi bi-arrow-left"
+                                viewBox="0 0 16 16"
+                                color="#fff"
+                            >
+                                <path
+                                fillRule="evenodd"
+                                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                                />
+                            </svg>
+                            <span> Continue Shopping</span>
+                        </button>
                     
                     </div>
                 </div>
