7d0d7a6d4a7fb5e70b1c582b256b4ed39cadd9f3
Omar Barakat <obarakat12@gmail.com>
2024-03-15 13:35:00 +0400

    create import images index

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,19 +1,23 @@
+// rentals slides
 import rBouncy from "../assets/images/rentals/Slides/1-Bouncy-Rental-Slide.jpg"
 import rPopcorn from "../assets/images/rentals/Slides/2-Popcorn-Station-Rental-Slide.jpg"
 import rBallPit from "../assets/images/rentals/Slides/3-Ball-Pit-Rental-Slide.jpg"
 import rCottonCandy from "../assets/images/rentals/Slides/4-Cotton-Candy-Station-Rental.jpg"
 import rBeautyBar from "../assets/images/rentals/Slides/5-Beauty-Bar-Rental.jpg"
 import rBubbleMachine from "../assets/images/rentals/Slides/6-Bubble-Machine-Rental.jpg"
+// rentals dec-cat
 import ribeauty from "../assets/images/rentals/Rentals/1. Beauty Bar/6.jpg"
 import ribouncy from "../assets/images/rentals/Rentals/2. Bouncy Houses/Overview 3.jpg"
 import ricandy from "../assets/images/rentals/Rentals/3. Cotton Candy Machine/ccm.jpg"
 import ricorn from "../assets/images/rentals/Rentals/4. Popcorn Machine/2.jpg"
 import ribubble from "../assets/images/rentals/Rentals/5. Bubble Machine/bubbleM.jpg"
+// home dec-cat
 import hmArches from "../assets/images/decorations/hm-arches.jpg"
 import hmCenterpieces from "../assets/images/decorations/hm-centerpieces.jpg"
 import hmColumns from "../assets/images/decorations/hm-columns.jpg"
 import hmGarlands from "../assets/images/decorations/hm-garlands.jpg"
 import hmSculptures from "../assets/images/decorations/hm-sculptures.jpg"
+// products
 
 
 
